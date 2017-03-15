83acaad27196aa1fd8dfccb244c0a1269340c774
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-14 17:54:15 -0700

    Made add student, confirmation, and added to student page, WIP

---
+++ hackbright-web.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github', 'jhacks')
+    first, last, github = hackbright.get_student_by_github(github)
+    rows = hackbright.get_grades_by_github(github)
+
+    return render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           rows=rows)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/new_student")
+def new_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("new_student.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first = request.form.get('first_name')
+    last = request.form.get('last_name')
+    github = request.form.get('github')
+    hackbright.make_new_student(first, last, github)
+    return render_template("confirmation.html",
+                           first=first,
+                           last=last,
+                           github=github)
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
